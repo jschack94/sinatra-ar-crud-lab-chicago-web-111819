6e24041fb2c08fe45dd21e26cde4a8a5278a3c7c
Jonathan Schack <jschack94@gmail.com>
2019-12-13 03:03:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,21 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+    end
+
+  get "/articles/new" do
+    @article = Article.new
+   erb :new
+end
+
+  post "/articles" do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{article.id}"
   end
 end
